9d9b48b386b07be53ed0f6813c393d0a82f835cc
DaniloParkour <danilomlcarvalho@gmail.com>
2024-02-17 15:39:17 -0300

    initial commit

---
+++ src/main/java/com/lanches/food_devlivery/FoodDevliveryApplication.java
@@ -0,0 +1,13 @@
+package com.lanches.food_devlivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDevliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDevliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lanches/food_devlivery/FoodDevliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lanches.food_devlivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDevliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
